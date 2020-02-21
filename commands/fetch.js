36f99f4b5f10864e8141dce9e930ca524365c4a9
Tails Era <TailsEraYT@hostingwebsites.tk>
2020-02-20 20:15:14 -0500

    Alpha 0.5 | Dummy Fetch command

---
+++ commands/fetch.js
@@ -0,0 +1,32 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            name: 'say',
+            enabled: true,
+            runIn: ['text', 'dm'],
+            cooldown: 60,
+            deletable: false,
+            bucket: 1,
+            aliases: [],
+            guarded: false,
+            nsfw: false,
+            permissionLevel: 0,
+            requiredPermissions: [],
+            requiredSettings: [],
+            subcommands: false,
+            description: "I'll Fetch the Latest Data!",
+            quotedStringSupport: false,
+            usage: '',
+            usageDelim: ` `,
+            extendedHelp: 'Go Make Fetch The Latest Data ASAP. (could of just waited every 5 Minutes)'
+        });
+    }
+
+    async run() {
+       message.send("Fetching Latest Data! You should See it in your desinated News Channel");
+    }
+
+};
